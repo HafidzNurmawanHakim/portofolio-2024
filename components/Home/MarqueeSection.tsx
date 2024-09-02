c3be8c740cc4e997dd804b8cd9ec59e60436961a
hafidznurmawanhakim <hafdznurmawan@gmail.com>
2024-09-02 15:59:27 +0700

    fixing light theme

--- components/Home/MarqueeSection.tsx
+++ components/Home/MarqueeSection.tsx
@@ -7,27 +7,27 @@ import Marquee from "../lib/Marquee";
 import { techData } from "@/config/site";
 
 function MarqueeSection() {
-  return (
-    <SmoothScrollbar>
-      {(bind) => (
-        <div {...bind}>
-          <Marquee reverse>
-            {techData.map((slide, index) => (
-              <div key={index} className="relative w-20 h-20 mx-6">
-                <Image fill alt="icons" className="w-10" src={slide.image} />
-              </div>
-            ))}
-          </Marquee>
-          <Marquee>
-            <h2>Turning ideas into reality</h2>
-          </Marquee>
-          <Marquee reverse>
-            <h2>I don&apos;t have a Team, I am the Team!</h2>
-          </Marquee>
-        </div>
-      )}
-    </SmoothScrollbar>
-  );
+   return (
+      <SmoothScrollbar>
+         {(bind) => (
+            <div {...bind}>
+               <Marquee reverse>
+                  {techData.map((slide, index) => (
+                     <div key={index} className="relative w-20 h-20 mx-6">
+                        <Image fill alt="icons" className="w-10" src={slide.image} />
+                     </div>
+                  ))}
+               </Marquee>
+               <Marquee>
+                  <h2 className="">Turning ideas into reality</h2>
+               </Marquee>
+               <Marquee reverse>
+                  <h2>I don&apos;t have a Team, I am the Team!</h2>
+               </Marquee>
+            </div>
+         )}
+      </SmoothScrollbar>
+   );
 }
 
 export default MarqueeSection;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import {
-  Navbar as NextUINavbar,
-  NavbarContent,
-  NavbarMenu,
-  NavbarMenuToggle,
-  NavbarBrand,
-  NavbarItem,
-  NavbarMenuItem,
+   Navbar as NextUINavbar,
+   NavbarContent,
+   NavbarMenu,
+   NavbarMenuToggle,
+   NavbarBrand,
+   NavbarItem,
+   NavbarMenuItem,
 } from "@nextui-org/navbar";
 import { Button } from "@nextui-org/button";
 import { Kbd } from "@nextui-org/kbd";
@@ -24,128 +24,123 @@ import { GithubIcon, HeartFilledIcon, SearchIcon } from "@/components/icons";
 import { fontFira } from "@/config/fonts";
 
 export const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
+   const searchInput = (
+      <Input
+         aria-label="Search"
+         classNames={{
+            inputWrapper: "bg-default-100",
+            input: "text-sm",
+         }}
+         endContent={
+            <Kbd className="hidden lg:inline-block" keys={["command"]}>
+               K
+            </Kbd>
+         }
+         labelPlacement="outside"
+         placeholder="Search..."
+         startContent={
+            <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+         }
+         type="search"
+      />
+   );
 
-  return (
-    <NextUINavbar
-      shouldHideOnScroll
-      classNames={{
-        wrapper: "rounded-full custom-blur",
-        base: "bg-transparent mt-2 relative z-50",
-      }}
-      isBlurred={false}
-      isMenuOpen={isMenuOpen}
-      maxWidth="xl"
-      onMenuOpenChange={(isOpen) => {
-        return setIsMenuOpen(isOpen);
-      }}
-    >
-      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <p className={clsx("font-bold text-inherit", fontFira.className)}>
-              V 1.0
-            </p>
-          </NextLink>
-        </NavbarBrand>
-      </NavbarContent>
-      <NavbarContent justify="center">
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <NextLink
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </NextLink>
-            </NavbarItem>
-          ))}
-        </ul>
-      </NavbarContent>
-
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
+   return (
+      <NextUINavbar
+         shouldHideOnScroll
+         classNames={{
+            wrapper: "rounded-full custom-blur",
+            base: "bg-transparent mt-2 relative z-50",
+         }}
+         isBlurred={false}
+         isMenuOpen={isMenuOpen}
+         maxWidth="xl"
+         onMenuOpenChange={(isOpen) => {
+            return setIsMenuOpen(isOpen);
+         }}
       >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-            <GithubIcon className="text-default-500" />
-          </Link>
-          {/* <ThemeSwitch /> */}
-        </NavbarItem>
-        {/* <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem> */}
-        <NavbarItem className="hidden md:flex">
-          <Button
-            // isExternal
-            as={Link}
-            className="text-sm font-normal text-default-600 bg-default-100"
-            // href={siteConfig.links.sponsor}
-            href={"#contact"}
-            startContent={<HeartFilledIcon className="text-danger" />}
-            variant="flat"
-          >
-            Hire Me
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
+         <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+            <NavbarBrand as="li" className="gap-3 max-w-fit">
+               <NextLink className="flex justify-start items-center gap-1" href="/">
+                  <p className={clsx("font-bold text-inherit", fontFira.className)}>V 1.0</p>
+               </NextLink>
+            </NavbarBrand>
+         </NavbarContent>
+         <NavbarContent justify="center">
+            <ul className="hidden lg:flex gap-4 justify-start ml-2">
+               {siteConfig.navItems.map((item) => (
+                  <NavbarItem key={item.href}>
+                     <NextLink
+                        className={clsx(
+                           linkStyles({ color: "foreground" }),
+                           "data-[active=true]:text-primary data-[active=true]:font-medium"
+                        )}
+                        color="foreground"
+                        href={item.href}
+                     >
+                        {item.label}
+                     </NextLink>
+                  </NavbarItem>
+               ))}
+            </ul>
+         </NavbarContent>
 
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-          <GithubIcon className="text-default-500" />
-        </Link>
-        <ThemeSwitch />
-        <NavbarMenuToggle />
-      </NavbarContent>
+         <NavbarContent className="hidden sm:flex basis-1/5 sm:basis-full" justify="end">
+            <NavbarItem className="hidden sm:flex gap-2">
+               <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+                  <GithubIcon className="text-default-500" />
+               </Link>
+               <ThemeSwitch />
+            </NavbarItem>
+            {/* <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem> */}
+            <NavbarItem className="hidden md:flex">
+               <Button
+                  // isExternal
+                  as={Link}
+                  className="text-sm font-normal text-default-600 bg-default-100"
+                  // href={siteConfig.links.sponsor}
+                  href={"#contact"}
+                  startContent={<HeartFilledIcon className="text-danger" />}
+                  variant="flat"
+               >
+                  Hire Me
+               </Button>
+            </NavbarItem>
+         </NavbarContent>
 
-      <NavbarMenu>
-        {searchInput}
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href={item.href}
-                size="lg"
-                onClick={() => setIsMenuOpen(false)}
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
-    </NextUINavbar>
-  );
+         <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+            <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+               <GithubIcon className="text-default-500" />
+            </Link>
+            <ThemeSwitch />
+            <NavbarMenuToggle />
+         </NavbarContent>
+
+         <NavbarMenu>
+            {searchInput}
+            <div className="mx-4 mt-2 flex flex-col gap-2">
+               {siteConfig.navMenuItems.map((item, index) => (
+                  <NavbarMenuItem key={`${item}-${index}`}>
+                     <Link
+                        color={
+                           index === 2
+                              ? "primary"
+                              : index === siteConfig.navMenuItems.length - 1
+                                ? "danger"
+                                : "foreground"
+                        }
+                        href={item.href}
+                        size="lg"
+                        onClick={() => setIsMenuOpen(false)}
+                     >
+                        {item.label}
+                     </Link>
+                  </NavbarMenuItem>
+               ))}
+            </div>
+         </NavbarMenu>
+      </NextUINavbar>
+   );
 };
